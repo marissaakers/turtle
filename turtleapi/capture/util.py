ee51e478b9f22149b9ced9351135a4c5be65dad4
Matthew Neidig <matthew@neidig.cc>
2020-01-30 13:29:38 -0500

    woops forgot a file

--- turtleapi/capture/util.py
+++ turtleapi/capture/util.py
@@ -1,10 +1,31 @@
 from turtleapi import db
-from turtleapi.models.turtlemodels import Turtle, Tag, TurtleSchema
+from turtleapi.models.turtlemodels import Turtle, Tag, TurtleSchema, TagSchema
 
+# Match one turtle
 def find_turtle_from_tags(tags):
     for tag in tags:
         turtle = Turtle.query.filter_by(turtle_id=tag['turtle']).first()
         if turtle is not None:
             turtle_schema = TurtleSchema()
             return turtle_schema.dump(turtle)
+    return None
+
+# Match all turtles
+def find_turtles_from_tags(tags):
+    #turtles = Turtle.query.filter(Turtle.turtle_id.in_(tags)).all()
+    tags = Tag.query.filter(Tag.tag_number.in_(tags)).all() #.distinct()
+    
+    print("TAGS IS")
+    print(tags)
+
+    if tags is not None:
+        tag_schema = TagSchema()
+        tagdump = tag_schema.dump(tags, many=True)
+        print(tagdump)
+
+        print(list(tagdump['turtle_id'].values()))
+
+        # if turtles is not None:
+        #     turtle_schema = TurtleSchema()
+        #     return turtle_schema.dump(turtles)
     return None
