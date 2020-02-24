6666ff142be83e3573f089dfaf41c1acdfb727cb
Matthew Neidig <matthew@neidig.cc>
2020-02-24 00:05:56 -0500

    WIP

--- turtleapi/capture/query_lagoon.py
+++ turtleapi/capture/query_lagoon.py
@@ -22,6 +22,7 @@ def query_lagoon(data):
     incidental_capture_schema = IncidentalCaptureSchema()
 
     ### Filters
+    FILTER_turtle_ids = data.get('turtle_ids', '')
     FILTER_tags = data.get('tags', '')
     FILTER_species = data.get('species', '') # Only match this species
 
@@ -42,14 +43,17 @@ def query_lagoon(data):
     FILTER_verified_by = data.get('verified_by', '')
     FILTER_investigated_by = data.get('investigated_by', '')
 
-
     ### End filters
 
+    if FILTER_turtle_ids == '' and FILTER_tags == '':
+        print("error: no turtle ids or tags provided to full lagoon query")
+        return {'error': 'no turtle ids or tags provided to full lagoon query'}
+    
     queries = []
 
     if FILTER_tags != '':
-        turtle_ids = find_turtles_from_tags(FILTER_tags)
-        queries.append(Turtle.turtle_id.in_(turtle_ids))
+        FILTER_turtle_ids = find_turtles_from_tags(FILTER_tags)
+        queries.append(Turtle.turtle_id.in_(FILTER_turtle_ids))
     if FILTER_species != '':
             queries.append(Turtle.species == FILTER_species)
 
@@ -58,72 +62,72 @@ def query_lagoon(data):
 
     # Make output object
     output = turtle_schema.dump(turtle_result, many=True)
-    t_counter = -1
+    # t_counter = -1
 
-    for turtle in turtle_result:
-        t_counter += 1
+    # for turtle in turtle_result:
+    #     t_counter += 1
 
-        turtle_id = turtle.turtle_id
-        t_output = output[t_counter]
-        t_output['turtle_id'] = turtle_id
-        del t_output['morphometrics']
+    #     turtle_id = turtle.turtle_id
+    #     t_output = output[t_counter]
+    #     t_output['turtle_id'] = turtle_id
+    #     del t_output['morphometrics']
 
-        # Grab tags
-        tag_result = Tag.query.filter_by(turtle_id=turtle_id).all()
-        t_output['tags'] = tag_schema.dump(tag_result, many=True)
+    #     # Grab tags
+    #     tag_result = Tag.query.filter_by(turtle_id=turtle_id).all()
+    #     t_output['tags'] = tag_schema.dump(tag_result, many=True)
 
-        ### Build list of queries
-        queries.clear()
-        queries.append(Encounter.turtle_id == turtle_id)
+    #     ### Build list of queries
+    #     queries.clear()
+    #     queries.append(Encounter.turtle_id == turtle_id)
 
-        if string_date_start != '':
-            queries.append(Encounter.encounter_date >= FILTER_encounter_date_start)
-        if string_date_end != '':
-            queries.append(Encounter.encounter_date <= FILTER_encounter_date_end)
-        if FILTER_investigated_by != '':
-            queries.append(Encounter.investigated_by == FILTER_investigated_by)
-        if FILTER_entered_by != '':
-            queries.append(Encounter.entered_by == FILTER_entered_by)
-        if FILTER_verified_by != '':
-            queries.append(Encounter.verified_by == FILTER_verified_by)
+    #     if string_date_start != '':
+    #         queries.append(Encounter.encounter_date >= FILTER_encounter_date_start)
+    #     if string_date_end != '':
+    #         queries.append(Encounter.encounter_date <= FILTER_encounter_date_end)
+    #     if FILTER_investigated_by != '':
+    #         queries.append(Encounter.investigated_by == FILTER_investigated_by)
+    #     if FILTER_entered_by != '':
+    #         queries.append(Encounter.entered_by == FILTER_entered_by)
+    #     if FILTER_verified_by != '':
+    #         queries.append(Encounter.verified_by == FILTER_verified_by)
         
-        ### Grab encounters, filter by anything appended above
-        encounter_result = Encounter.query.filter(*queries).all()
+    #     ### Grab encounters, filter by anything appended above
+    #     encounter_result = Encounter.query.filter(*queries).all()
 
-        if not encounter_result:
-            del output[t_counter]
-            t_counter -= 1
-            continue
-        t_output['encounters'] = lagoon_encounter_schema.dump(encounter_result, many=True)
-        e_counter = -1
+    #     if not encounter_result:
+    #         del output[t_counter]
+    #         t_counter -= 1
+    #         continue
+    #     t_output['encounters'] = lagoon_encounter_schema.dump(encounter_result, many=True)
+    #     e_counter = -1
 
-        for encounter in encounter_result:
-            e_counter += 1
+    #     for encounter in encounter_result:
+    #         e_counter += 1
 
-            encounter_id = encounter.encounter_id
-            e_output = t_output['encounters'][e_counter]
+    #         encounter_id = encounter.encounter_id
+    #         e_output = t_output['encounters'][e_counter]
 
-            # Grab morphometrics
-            morphometrics_result = Morphometrics.query.filter_by(encounter_id=encounter_id).first() # Only one morph per encounter ya?
-            e_output['morphometrics'] = morphometrics_schema.dump(morphometrics_result)
+    #         # Grab morphometrics
+    #         morphometrics_result = Morphometrics.query.filter_by(encounter_id=encounter_id).first() # Only one morph per encounter ya?
+    #         e_output['morphometrics'] = morphometrics_schema.dump(morphometrics_result)
 
-            # Grab metadata
-            metadata_id = encounter.metadata_id
-            metadata_result = Metadata.query.filter_by(metadata_id=metadata_id).first()
-            e_output['metadata'] = metadata_schema.dump(metadata_result)
-            del e_output['metadata']['encounters']
+    #         # Grab metadata
+    #         metadata_id = encounter.metadata_id
+    #         metadata_result = Metadata.query.filter_by(metadata_id=metadata_id).first()
+    #         e_output['metadata'] = metadata_schema.dump(metadata_result)
+    #         del e_output['metadata']['encounters']
 
-            # Grab samples
-            samples_result = Sample.query.filter_by(encounter_id=encounter_id).all()
-            e_output['samples'] = sample_schema.dump(samples_result, many=True)
+    #         # Grab samples
+    #         samples_result = Sample.query.filter_by(encounter_id=encounter_id).all()
+    #         e_output['samples'] = sample_schema.dump(samples_result, many=True)
 
-            # Grab nets
-            nets_result = Net.query.filter_by(metadata_id=metadata_id).all()
-            e_output['metadata']['nets'] = net_schema.dump(nets_result, many=True)
+    #         # Grab nets
+    #         nets_result = Net.query.filter_by(metadata_id=metadata_id).all()
+    #         e_output['metadata']['nets'] = net_schema.dump(nets_result, many=True)
 
-            # Grab incidental captures
-            incidental_captures_result = IncidentalCapture.query.filter_by(metadata_id=metadata_id).all()
-            e_output['metadata']['incidental_captures'] = incidental_capture_schema.dump(incidental_captures_result, many=True)
+    #         # Grab incidental captures
+    #         incidental_captures_result = IncidentalCapture.query.filter_by(metadata_id=metadata_id).all()
+    #         e_output['metadata']['incidental_captures'] = incidental_capture_schema.dump(incidental_captures_result, many=True)
 
     return output
 
