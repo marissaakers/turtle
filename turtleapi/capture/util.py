a30b9165ec3dd35e9a1e2a9cf12a124b5a4ea1b8
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2020-03-02 16:17:06 -0500

    Update util.py

--- turtleapi/capture/util.py
+++ turtleapi/capture/util.py
@@ -1,5 +1,7 @@
 from turtleapi import db
 from turtleapi.models.turtlemodels import Turtle, Tag, TurtleSchema, TagSchema
+import json
+from flask import jsonify
 
 # Match one turtle
 def find_turtle_from_tags(tags):
@@ -15,9 +17,9 @@ def find_turtle_from_tags(tags):
     # return None
 
     for t in tags:
-        res = Tag.query.filter_by(tag_number=t['tag_number']).first()
+        res = db.session.query(Tag).filter(Tag.tag_number==t['tag_number']).first()
         if res is not None:
-            turtle = Turtle.query.filter_by(turtle_id=res.turtle_id).first()
+            turtle = db.session.query(Turtle).filter(Turtle.turtle_id==res.turtle_id).first()
             return turtle
     return None
 
@@ -37,18 +39,33 @@ def find_turtles_from_tags(tags):
     return None
 
 # For editing, insert any new tags
-def insert_new_tags(tags):
-    tag_schema = TagSchema()
+# WIP & untested 
+# def insert_new_tags(turtle_id, tags):
+#     turtle_result = Tag.query.filter(Tag.turtle_id == turtle_id)
     
-    print("HEYO")
-    print(tags)
-    for t in tags:
-        print(t)
-        tag_result = Tag.query.filter(Tag.tag_number==t['tag_number']).first()
-        print(tag_result)
-        if tag_result is None:
-            print(True)
-            tag = tag_schema.load(t, unknown='EXCLUDE')
-            db.session.add(tag)
+#     if turtle_result is not None:
+#         tag_schema = TagSchema()
+#         turtle_result_dump = tag_schema.dump(turtle_result, many=True)
+#         turtle_tag_list = [d['tag_number'] for d in turtle_result_dump]
+#         for t in tags:
+#             if t not in turtle_tag_list:
+#                 new_tag = Tag(
+#                 turtle=turtle_id,
+#                 tag_number=t,
+#                 location="DEBUG",
+#                 active=tag['active'],
+#                 tag_type=tag['tag_type'] #grrr
+#             )
+        
+def my_custom_serializer(value, **kwargs):
+    filter_fields = kwargs.pop("filter_fields", None)
+    result = {}
+    for field in filter_fields:
+        result[field] = value.get(field, None)
 
-    db.session.commit()
+    return result
+#    return json.dumps(result)
+
+def date_handler(obj):
+        if hasattr(obj, 'isoformat'):
+            return obj.isoformat()
