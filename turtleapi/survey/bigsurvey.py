685aa08235a005493923f15137ea24f5b848c7f1
Adam Daugherty <adaugherty42@gmail.com>
2020-03-25 23:26:52 -0400

    fixed deserialization for midreach insert

--- turtleapi/survey/bigsurvey.py
+++ turtleapi/survey/bigsurvey.py
@@ -1,5 +1,5 @@
 from turtleapi import db
-from turtleapi.models.turtlemodels import BigSurvey
+from turtleapi.models.turtlemodels import BigSurvey, Emergence
 
 def insert_pafb(data):
     pafb = BigSurvey.new_from_dict(data)
@@ -8,7 +8,17 @@ def insert_pafb(data):
     return {'message':'no errors'}
 
 def insert_mid_reach(data):
-    mid_reach = BigSurvey.new_from_dict(data)
+    mid_reach = BigSurvey()
+    emergences = []
+
+    for item in data.items():
+        if item[0] == 'emergences':
+            for em in item[1]:
+                emergences.append(Emergence(**em))
+        else:
+            setattr(mid_reach, item[0], item[1])
+
+    mid_reach.emergences = emergences
     db.session.add(mid_reach)
     db.session.commit()
     return {'message':'no errors'}
