bd2ce3bd2c1aac6be1543092fdc7d3c0cb1ec502
Adam Daugherty <adaugherty42@gmail.com>
2020-04-11 13:40:21 -0400

    fixed sample tracking on backend

--- turtleapi/capture/sample_tracking.py
+++ turtleapi/capture/sample_tracking.py
@@ -3,7 +3,7 @@ from turtleapi.models.turtlemodels import Sample, SampleTracking
 from sqlalchemy.orm import load_only, defer
 
 def add_tracking_entry(data):
-    sample = Sample.query.filter_by(sample_id=data['sample_id']).first()
+    sample = db.session.query(Sample).filter_by(sample_id=data['sample_id']).first()
 
     entry = SampleTracking(
         sample=sample,
@@ -15,7 +15,7 @@ def add_tracking_entry(data):
     return 'Successfully added tracking entry'
 
 def update_tracking_entry(sample_tracking_id, data):
-    entry = SampleTracking.query.filter_by(sample_tracking_id=sample_tracking_id).first()
+    entry = db.session.query(SampleTracking).filter_by(sample_tracking_id=sample_tracking_id).first()
     entry.date = data['date']
     entry.notes = data['notes']
     db.session.commit()
@@ -23,10 +23,9 @@ def update_tracking_entry(sample_tracking_id, data):
 
 def get_sample(sample_id):
     query_result = db.session.query(Sample).filter_by(sample_id=sample_id).first()
-    return query_result.to_dict(max_nesting=1)
+    return query_result.to_dict(max_nesting=2)
 
 def delete_tracking_entry(sample_tracking_id):
-    print(sample_tracking_id)
-    SampleTracking.query.filter_by(sample_tracking_id=sample_tracking_id).delete()
+    db.session.query(SampleTracking).filter_by(sample_tracking_id=sample_tracking_id).delete()
     db.session.commit()
     return 'Successfully deleted tracking entry'
